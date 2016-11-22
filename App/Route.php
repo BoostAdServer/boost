c0e3b259eda409499ace761a09bdc9c394ee3979
Nicolás Seijas <nicolas@dinvaders.com>
2016-11-22 14:58:15 -0300

    Adding new methods

--- App/Route.php
+++ App/Route.php
@@ -14,34 +14,58 @@ class Route {
 
     /**
      * List of allowed access to the app
-     * @var array
+     * @var    array
+     * @access private
      */
     private $routes = [
-        'advertiser-campaigns',
-        'advertiser',
-        'campaign-edit',
-        'campaign-banners',
-        'stats'
+        'advertiser-campaigns' => 'advertiser-campaigns/{advertiser}',
+        'advertiser-edit'      => 'advertiser-edit/{advertiser}',
+        'advertiser'           => 'advertiser',
+        'campaign-edit'        => 'campaign-edit/{advertiser}/{campaign}',
+        'campaign-banners'     => 'campaign-banners',
+        'banner-edit'          => 'banner-edit/{advertiser}/{campaign}/{banner}',
+        'stats'                => 'stats'
     ];
 
     function __construct() {
 
         $request_uri   = @parse_url( $_SERVER['REQUEST_URI'] );
         $path          = $request_uri['path'];
-        $regex         = '/\/dashboard\/([A-Za-z\-]+)\/([0-9]+)?/';
+        $regex         = '/\/dashboard\/([A-Za-z\-]+)\/?([0-9]+)?/';
 
         if( preg_match( $regex, $path, $match ) ) :
 
             $this->get( $match );
 
+        else :
+
+            $this->get( 'home' );
+
         endif;
 
     }
 
+    /**
+     *
+     *
+     */
     function get( array $match ) {
 
         $routes = $this->routes;
-        var_dump( $routes );
+
+        if( array_key_exists( $match[1], $routes ) ) :
+            require "Template/header.html";
+            require "Template/{$match[1]}.html";
+            require "Template/footer.html";
+        endif;
+
+    }
+
+    /**
+     *
+     *
+     */
+    private function parse( $element ) {
 
     }
 
